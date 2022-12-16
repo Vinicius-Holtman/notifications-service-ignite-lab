a7e5dd3dbd4fd7cc83993efbcd81193f524766f9
Vinicius <viniholtman28@gmail.com>
2022-12-16 12:05:30 -0300

    Created view model

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/SendNotification';
 import { CraeteNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from "../view-models/notification-view-model"
 
 @Controller()
 export class NotificationsController {
@@ -16,6 +17,6 @@ export class NotificationsController {
       category
     })
 
-    return { notification }
+    return { notification: NotificationViewModel.toHTTP(notification) }
   }
 }
