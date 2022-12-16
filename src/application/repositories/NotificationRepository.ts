ab1f655cee36d36991b68ee71c1bad671a2353d9
Vinicius <viniholtman28@gmail.com>
2022-12-16 15:51:49 -0300

    Fixed error name

--- src/application/repositories/NotificationRepository.ts
+++ src/application/repositories/NotificationRepository.ts
@@ -5,5 +5,5 @@ export abstract class NotificationRepository {
   abstract findById(notificationId: string): Promise<Notification | null>;
   abstract save(notifiaction: Notification): Promise<void>;
   abstract countManyByRecipientId(recipientId: string): Promise<number>;
-  abstract findManyBtRecipientId(recipientId: string): Promise<Notification[]>
+  abstract findManyByRecipientId(recipientId: string): Promise<Notification[]>
 }
